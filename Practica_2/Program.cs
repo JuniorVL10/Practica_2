3def728f7f10b553c588faadcedc517526249af6
JuniorVL10 <joseluisvallejolazcano@gmail.com>
2021-02-23 18:48:42 -0800

    First Commit

---
+++ Practica_2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Mostrar Hellow world en la consola
+            System.Console.WriteLine("Hello World");
+            Console.ReadLine();
+        }
+    }
+}
