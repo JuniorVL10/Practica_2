2b4f0f4740e79e6f30fd73869cf754572b1fe9fb
JuniorVL10 <joseluisvallejolazcano@gmail.com>
2021-03-04 18:27:47 -0800

    Programa que pide el nombre y la edad.

---
+++ Ejersicio_String2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejersicio_String2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            String Nombre;
+            string LineaEdad;
+            int edad;
+            Console.WriteLine("Pedir Nombre");
+            Nombre = Console.ReadLine();
+            Console.WriteLine("Escribe la edad");
+            LineaEdad = Console.ReadLine();
+            edad = Convert.ToInt32(LineaEdad);
+            Console.WriteLine("Tu nombre es:  " + Nombre + "y tiene" + edad + "años");
+            Console.WriteLine("Presiona una tecla");
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
